050ae660869729264484275f8b5d922daebf752d
MKaramen <matis.kara@gmail.com>
2019-11-14 16:17:49 +0100

    change confidence lvl required

--- assets/script/sketch.js
+++ assets/script/sketch.js
@@ -22,115 +22,115 @@ let count = 0;
 
 // Load the model first
 function preload() {
-  classifier = ml5.imageClassifier(imageModelURL + "model.json");
+    classifier = ml5.imageClassifier(imageModelURL + "model.json");
 }
 
 function setup() {
-  noCanvas();
-  // Create the video
-  video = createCapture(VIDEO);
-  video.size(320, 240);
-  video.hide();
+    noCanvas();
+    // Create the video
+    video = createCapture(VIDEO);
+    video.size(320, 240);
+    video.hide();
 
-  flippedVideo = ml5.flipImage(video);
-  // Start classifying
-  classifyVideo();
+    flippedVideo = ml5.flipImage(video);
+    // Start classifying
+    classifyVideo();
 
-  // Load the brain.rive file
-  bot
-    .loadFile("assets/riveScript/brain.rive")
-    .then(loading_done)
-    .catch(loading_error);
+    // Load the brain.rive file
+    bot
+        .loadFile("assets/riveScript/brain.rive")
+        .then(loading_done)
+        .catch(loading_error);
 
-  inputElement.addEventListener("keyup", () => {
-    clearTimeout(typingTimer);
-    typingTimer = setTimeout(chat, doneTypingInterval);
-  });
+    inputElement.addEventListener("keyup", () => {
+        clearTimeout(typingTimer);
+        typingTimer = setTimeout(chat, doneTypingInterval);
+    });
 
-  inputElement.addEventListener("keydown", () => {
-    clearTimeout(typingTimer);
-  });
+    inputElement.addEventListener("keydown", () => {
+        clearTimeout(typingTimer);
+    });
 
-  inputElement.addEventListener("click", () => {
-    inputElement.value = "";
-  });
+    inputElement.addEventListener("click", () => {
+        inputElement.value = "";
+    });
 
-  inputElement.addEventListener("change", () => {
-    console.log("yes");
-  });
+    inputElement.addEventListener("change", () => {
+        console.log("yes");
+    });
 }
 
 function draw() {
-  switch (label) {
-    case "":
-      break;
-    case "Location":
-      inputElement.value = "location";
-      chat();
-      label = "";
-      break;
-    case "die":
-      inputElement.value = "die";
-      label = "";
-      chat();
-      break;
-    case "heart":
-      inputElement.value = "kind";
-      label = "";
-      chat();
-      break;
-  }
+    switch (label) {
+        case "":
+            break;
+        case "Location":
+            inputElement.value = "location";
+            chat();
+            label = "";
+            break;
+        case "die":
+            inputElement.value = "die";
+            label = "";
+            chat();
+            break;
+        case "heart":
+            inputElement.value = "kind";
+            label = "";
+            chat();
+            break;
+    }
 }
 
 // Get a prediction for the current video frame
 function classifyVideo() {
-  flippedVideo = ml5.flipImage(video);
-  classifier.classify(flippedVideo, gotResult);
+    flippedVideo = ml5.flipImage(video);
+    classifier.classify(flippedVideo, gotResult);
 }
 
 // When we get a result
 function gotResult(error, results) {
-  // If there is an error
-  if (error) {
-    console.error(error);
-    return;
-  }
+    // If there is an error
+    if (error) {
+        console.error(error);
+        return;
+    }
 
-  // The results are in an array ordered by confidence.
-  // console.log(results[0])
-  if (results[0].confidence > 0.99) {
-    label = results[0].label;
-  }
+    // The results are in an array ordered by confidence.
+    // console.log(results[0])
+    if (results[0].confidence > 0.90) {
+        label = results[0].label;
+    }
 
-  if (count == 0) {
-    document.getElementById("loading").classList.add("none");
-    count++;
-  }
+    if (count == 0) {
+        document.getElementById("loading").classList.add("none");
+        count++;
+    }
 
-  // Classifiy again!
-  classifyVideo();
+    // Classifiy again!
+    classifyVideo();
 }
 
 const chat = () => {
-  bot.sortReplies();
-  let username = "local-user";
-  let input = inputElement.value;
-  if (input != "") {
-    bot.reply(username, input).then(reply => {
-      output.innerHTML = reply;
-      setTimeout(() => {
-        output.innerHTML = "";
-        inputElement.value = "";
-      }, 2000);
-    });
-  }
+    bot.sortReplies();
+    let username = "local-user";
+    let input = inputElement.value;
+    if (input != "") {
+        bot.reply(username, input).then(reply => {
+            output.innerHTML = reply;
+            setTimeout(() => {
+                output.innerHTML = "";
+                inputElement.value = "";
+            }, 2000);
+        });
+    }
 };
 
 // Bouffe allergie
 //Vegan
 
 function loading_done() {
-  console.log("Brain ready");
+    console.log("Brain ready");
 }
 
 // It's good to catch errors too!
